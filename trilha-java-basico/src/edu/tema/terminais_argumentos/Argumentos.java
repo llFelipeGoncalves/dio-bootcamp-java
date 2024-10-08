77935f87df6b0e121a9388b696e2836733c3ba81
ll.Felipe <luizfelipe.goncalves442@gmail.com>
2024-10-08 14:48:26 -0300

    docs: add Scanner

---
+++ trilha-java-basico/src/edu/tema/terminais_argumentos/Argumentos.java
@@ -0,0 +1,14 @@
+package edu.tema.terminais_argumentos;
+import java.util.Scanner;
+
+public class Argumentos {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um nome");
+        String nome = scanner.next();
+
+        System.out.println("meu nome é: " + nome);
+    }    
+}
