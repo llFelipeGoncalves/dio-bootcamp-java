5ba4896481a7e35f3354a4b6f7ffd5da2d13d3fb
ll.Felipe <luizfelipe.goncalves442@gmail.com>
2024-10-08 00:52:54 -0300

    docs: update

---
+++ trilha-java-basico/src/edu/tema/operadores/Logicos.java
@@ -0,0 +1,19 @@
+package edu.tema.operadores;
+
+public class Logicos {
+    public static void main(String[] args) {
+        
+        boolean condicao1 = true;
+        
+        boolean condicao2 = true;
+        
+        if (condicao1 && condicao2){
+            System.out.println("as duas sao verdadeiras");
+        }
+        
+        if (condicao1 || condicao2){
+            System.out.println("uma ou outra é verdadeira");
+        }
+
+    }
+}
---
+++ trilha-java-basico/src/edu/tema/operadores/Relacionais.java
@@ -0,0 +1,21 @@
+package edu.tema.operadores;
+
+public class Relacionais {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        boolean simNao = a == b; // iguais
+        
+        System.out.println(simNao); 
+
+        simNao = a != b; // diferente
+        
+        System.out.println(simNao);
+
+        simNao = a >= b; // maior ou igual
+        
+        System.out.println(simNao);
+
+    }
+}
